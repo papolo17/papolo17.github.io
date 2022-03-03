0f98999780674ddde5bdc7b65ba97faea3d86eb4
papolo17 <wwkeyrindc2015@gmail.com>
2022-03-02 16:44:58 -1200

    nuevo proyecto

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var boton = document.getElementById('boton');
+var checks = document.querySelectorAll('.valores');
+var wrap = document.querySelector('.wrap');
+var A = document.getElementById('seccionA');
+var B = document.getElementById('seccionB');
+var C = document.getElementById('seccionC');
+
+
+
+
+boton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    checks.forEach( (e) =>{
+        if(e.checked === true){
+            localStorage.setItem('info', e.value)
+        }
+        
+    });
+     
+    if(informacion === 'seccion1'){
+        wrap.style.display = 'none';
+        A.style.display = 'block'
+    
+    }
+    if(informacion === 'seccion2'){
+        wrap.style.display = 'none';
+        B.style.display = 'block'
+
+    }
+    if(informacion === 'seccion3'){
+        wrap.style.display = 'none';
+        C.style.display = 'block'
+
+    }
+    
+    
+    
+});
+
+
+const informacion = localStorage.getItem('info');
+
+
+// sistema de carga 
+window.addEventListener('load', () => {
+    const contenedor = document.querySelector('.contenedor')
+    contenedor.style.opacity = 0
+    contenedor.style.visibility = 'hidden'
+});
+        
+        
